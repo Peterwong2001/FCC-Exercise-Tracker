1a0276d2530bf1bcfcdc6243efde0dc64de7e2fc
Glitch (futuristic-inquisitive-room) <none>
2024-02-25 09:45:07 +0000

    🎐👶 Checkpoint ./index.js:65325151/575

--- index.js
+++ index.js
@@ -104,9 +104,34 @@ app.get("/api/users/:_id/logs", function(req, res) {
   const { from, to, limit } = req.query;
   const user = User.findById(id);
   
-  if(!user) {
-    res.send()
+  let dateObj = {}
+  if (from) {
+    dateObj["$gte"] = new Date(from)
   }
+  if (to) {
+    dateObj["$lte"] = new Date(to)
+  }
+  let filter = {
+    user_id: id
+  }
+  if (from || to) {
+    filter.date = dateObj;
+  }
+  
+  const exercises = Tracker.find(filter).limit()
+  
+  const log = exercises.map(e => ({
+    description: e.description,
+    duration: e.duration,
+    date: e.date.toDateString()
+  }))
+  
+  res.json({
+    username: user.username,
+    count: exercises.length,
+    _id: user._id,
+    log
+  })
   
 })
 
