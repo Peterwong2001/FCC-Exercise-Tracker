1ce6fe77bef89fb61d31c2074b3ab7d5912e563d
Glitch (futuristic-inquisitive-room) <none>
2024-02-26 10:57:52 +0000

    🌵🏔 Checkpoint ./index.js:65325151/106

--- index.js
+++ index.js
@@ -121,7 +121,7 @@ app.get("/api/users/:_id/logs", function(req, res) {
         date: a.date
       }))
       
-     if (responseObj.log.date === ) {
+     if (Symbol.for(responseObj.log.date) === Symbol.for("Invalid Date")) {
          responseObj.log.date = new Date().toDateString()
          }
       
